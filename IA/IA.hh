b800991baad42b23c6215495023db84f3450a0d6
gazzol_j <gazzol_j@epitech.eu>
2015-05-12 13:06:09 +0200

    dossier IA

---
+++ IA/IA.hh
@@ -0,0 +1,21 @@
+//
+// IA.hh for IA_bomber in /home/gazzol_j/rendu/cpp_bomberman/IA
+// 
+// Made by julien gazzola
+// Login   <gazzol_j@epitech.net>
+// 
+// Started on  Tue May 12 09:55:35 2015 julien gazzola
+// Last update Tue May 12 13:05:10 2015 julien gazzola
+//
+
+#ifndef IA_HH_
+# define IA_HH_
+
+class IA
+{
+public:
+  IA()
+  ~IA()
+};
+
+#endif // !IA_HH_
