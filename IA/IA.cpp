8924610a7c75a2fd557e275ee42591635c19ec77
gazzol_j <gazzol_j@epitech.eu>
2015-06-08 17:31:22 +0200

    ia de droite

--- IA/IA.cpp
+++ IA/IA.cpp
@@ -1,5 +1,6 @@
 
 # include <vector>
+# include "Map.hh"
 # include "Definition.hh"
 # include "Player.hh"
 # include "Bomb.hh"
@@ -10,100 +11,65 @@
 # include "IA.hh"
 
 IA::IA(){
-
 }
 
 IA::~IA(){
 }
 
-
-t_input		IA::FindBonus(Map &map, Player *player){
-  if (CheckBonus(map, player) != 0)
-    return (Move(map, player));
-  else
-    return (CheckPlayer(map, player));
-}
-
-t_input		IA::doAction(Map &map, Player *player){
-  std::vector<std::pair<AObject*, int> >	tab;
+t_input			IA::doAction(Map &map, Player *player){
+  std::vector<int>	tab;
 
   tab = CreateTable(map, player);
-  if (CheckBomb(map, player) != 0)
-    return (CheckFreeCase(map, player));
-  else
-    return (FindBonus(map, player));
-  std::cout << "caca" << std::endl;
-  return (MOVE_RIGHT);
+  return (Move(tab, map, player));
 }
 
-t_input       	IA::CheckFreeCase(Map &map, Player *player){
-  (void) map;
-  (void) player;
-  return (MOVE_RIGHT);
+t_input				IA::Move(std::vector<int> tab, Map &map, Player *player){
+  int				high = 0;
+  std::vector<int>::iterator	it;
+
+  CreateVectorPos();
+  for (it = tab.begin(); it != tab.end(); ++it)
+    if (*it > high)
+      high = *it;
+  return (checkPos(it, map, player, tab));
 }
 
-int       	IA::CheckBonus(Map &map, Player *player){
-  (void) map;
-  (void) player;
-  return (1);
-}
+std::vector<int>		IA::CreateTable(Map &map, Player *player){
+    std::vector<int>		tab;
+    std::pair<int, int>		posPlayer;
+    std::pair<int, int>		tmp;
+    AObject			*obj;
+    int				i = 0;
 
-t_input       	IA::CheckPlayer(Map &map, Player *player){
-  (void) map;
-  (void) player;
-  /*
-    if (playerProche)
-       ;
-    else
-       return ();
-   */
-  return (MOVE_RIGHT);
-}
-
-t_input		IA::Move(Map &map, Player *player){
-  (void) map;
-  (void) player;
-  return (MOVE_RIGHT);
-}
-
-std::vector<std::pair<AObject*, int> >		IA::CreateTable(Map &map, Player *player){
-    std::vector<std::pair<AObject*, int> >	tab;
-    std::pair<AObject*, int>			pairToAdd;
-    std::pair<int, int>				posPlayer;
-    std::pair<int, int>				tmp;
-    AObject					*obj;
-
+    for(int i = 0; i != 25; ++i)
+      tab.push_back(-1000);
     posPlayer = player->getPos();
     tmp.first = posPlayer.first - 2;
     tmp.second = posPlayer.second - 2;
-    if (posPlayer.first - 2 >= 0){
-      if (posPlayer.second - 2 >= 0){
-	for (unsigned int y = 0; y != 5; ++y){
-	  for (unsigned int x = 0; x != 5; ++x){
+    for (unsigned int y = 0; y != 5; ++y){
+      if (tmp.second >= 0 && tmp.second < map.getSize().second){
+	for (unsigned int x = 0; x != 5; ++x){
+	  if (tmp.first >= 0 && tmp.first < map.getSize().first){
 	    obj = map.getItemAtPos(tmp);
-	    pairToAdd.second = obj->getSmell();
-	    tab.push_back(pairToAdd);
+	    tab[i] = obj->getSmell();
+	    ++i;
 	    ++tmp.first;
 	  }
-	  tmp.first = posPlayer.first - 2;
-	  ++tmp.second;
 	}
       }
+      tmp.first = posPlayer.first - 2;
+      ++tmp.second;
     }
     return (tab);
 }
 
 int			IA::CheckBomb(Map &map, Player *player){
   int			i = 0;
-
   
-  if ((i = checkBottom(map, player)) == 1)
-    return (1);
-  if ((i = checkUp(map, player)) == 1)
-    return (1);
-  if ((i = checkRight(map, player)) == 1)
-    return (1);
-  if ((i = checkLeft(map, player)) == 1)
+  if ((i = checkBottom(map, player)) == 1 
+      || (i = checkUp(map, player)) == 1
+      || (i = checkRight(map, player)) == 1 
+      || (i = checkLeft(map, player)) == 1)
     return (1);
   return (0);
 }
